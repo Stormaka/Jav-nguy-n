706a4028d908cf521cdbaf19eea7edbe72fa47ea
Stormaka <nguyenstorm2k5@gmail.com>
2025-07-29 14:36:33 +0700

    Update Cau2giuaki.java

--- Cau2giuaki.java
+++ Cau2giuaki.java
@@ -1,62 +1,31 @@
-interface IShape {
-    double PI = 3.14;
-    double getArea();
-    double getPerimeter();
-    void showInfo();
-}
-
-class Circle implements IShape {
-    private double radius;
-
-    public Circle(double radius) {
-        this.radius = radius;
-    }
-
-    public double getArea() {
-        return PI * radius * radius;
-    }
-
-    public double getPerimeter() {
-        return 2 * PI * radius;
-    }
-
-    public void showInfo() {
-        System.out.println("Radius: " + radius);
-        System.out.println("Area: " + getArea());
-        System.out.println("Perimeter: " + getPerimeter());
-    }
-}
-
-class Rectangle implements IShape {
-    private double width;
-    private double length;
-
-    public Rectangle(double width, double length) {
-        this.width = width;
-        this.length = length;
-    }
-
-    public double getArea() {
-        return width * length;
-    }
-
-    public double getPerimeter() {
-        return 2 * (width + length);
-    }
-
-    public void showInfo() {
-        System.out.println("Width: " + width);
-        System.out.println("Length: " + length);
-        System.out.println("Area: " + getArea());
-        System.out.println("Perimeter: " + getPerimeter());
-    }
-}
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        IShape circle = new Circle(25.5);
-        IShape rectangle = new Rectangle(5, 12);
-        circle.showInfo();
-        rectangle.showInfo();
+        LinkedList<Integer> list = new LinkedList<>();
+        Random rand = new Random();
+
+        // Bước 1: Tạo LinkedList chứa 8 số nguyên < 100
+        for (int i = 0; i < 8; i++) {
+            list.add(rand.nextInt(100)); // Số từ 0 đến 99
+        }
+        System.out.println("Danh sách ban đầu: " + list);
+
+        // Bước 2: Thêm phần tử 25 vào vị trí số 2
+        list.add(2, 25);
+        System.out.println("Sau khi thêm 25 vào vị trí 2: " + list);
+
+        // Bước 3: Xoá phần tử có giá trị < 30
+        list.removeIf(n -> n < 30);
+        System.out.println("Sau khi xoá phần tử < 30: " + list);
+
+        // Bước 4: In vị trí của phần tử có giá trị từ 40 đến 80
+        System.out.print("Vị trí các phần tử có giá trị từ 40 đến 80: ");
+        for (int i = 0; i < list.size(); i++) {
+            int value = list.get(i);
+            if (value >= 40 && value <= 80) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
